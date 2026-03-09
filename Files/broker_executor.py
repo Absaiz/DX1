8e8a86f11b505857242743dce4248e7e3f282116
Absaiz <absaiz@gmail.com>
2026-03-09 20:24:25 +0100

    Update broker_executor.py

--- Files/broker_executor.py
+++ Files/broker_executor.py
@@ -2,3 +2,15 @@
 self.col_res.insert("--------------------------------")
 self.col_res.insert("HOLA: EL SCRIPT ESTA VIVO")
 self.col_res.insert("--------------------------------")
+# Test directo
+self.col_res.insert("--------------------------------")
+self.col_res.insert("HOLA: EL SCRIPT ESTA VIVO")
+self.col_res.insert("--------------------------------")
+# Test directo
+self.col_res.insert("--------------------------------")
+self.col_res.insert("HOLA: EL SCRIPT ESTA VIVO")
+self.col_res.insert("--------------------------------")
+# Test directo
+self.col_res.insert("--------------------------------")
+self.col_res.insert("HOLA: EL SCRIPT ESTA VIVO")
+self.col_res.insert("--------------------------------")
