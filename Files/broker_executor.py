fb734cef84ec16865259f55f44ce14ffd60101ca
Absaiz <absaiz@gmail.com>
2026-03-09 11:05:05 +0100

    Update broker_executor.py

--- Files/broker_executor.py
+++ Files/broker_executor.py
@@ -1,45 +1,48 @@
-# broker_executor.py - VERSIÓN 012 (FIX SCOPE & HERCULES)
+# broker_executor.py - VERSIÓN 013 (ELIMINANDO SCOPE ERRORS)
 import threading
-import time
+import socket
 
-# Forzamos el log local para saber que hemos arrancado
-self.log.info("--- [LOG 012] REPARANDO SCOPE Y CONECTANDO A HÉRCULES ---")
+# Definimos la IP de tu PC fuera para que sea fácil de cambiar
+MI_PC = "192.168.171.156"
 
-def send_hercules(msg):
-    import socket # Import local para evitar 'not defined'
+self.log.info("--- [LOG 013] BLINDANDO FUNCIONES ---")
+
+def servidor_total(comp, ip_destino):
+    import socket
+    import time
+
+    def log_externo(texto):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            s.connect((ip_destino, 23))
+            s.sendall(f"DX1_LOG: {texto}\n".encode())
+            s.close()
+        except:
+            pass
+
+    log_externo("INICIANDO HILO DE RED 013")
+    
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(2)
-        s.connect(("192.168.171.156", 23))
-        s.sendall(f"DX1_SUDO: {msg}\n".encode())
-        s.close()
-    except:
-        pass
-
-def broker_loop(comp):
-    import socket # Import local vital
-    comp.log.info("Iniciando bucle de red en 1888...")
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('0.0.0.0', 1888))
-        s.listen(5)
-        send_hercules("!!! PUERTO 1888 ABIERTO EN DX1 !!!")
+        # Intentamos abrir el puerto 1888
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(('0.0.0.0', 1888))
+        server.listen(5)
+        log_externo("EXITO: Puerto 1888 abierto")
+        comp.log.info("!!! PUERTO 1888 ONLINE !!!")
         
         while True:
-            conn, addr = s.accept()
-            msg = f"CONEXION DESDE: {addr}"
-            send_hercules(msg)
-            comp.log.info(msg)
+            conn, addr = server.accept()
+            log_externo(f"CONEXION ENTRANTE DESDE {addr}")
             conn.close()
     except Exception as e:
-        error_msg = f"ERROR BROKER: {str(e)}"
-        comp.log.error(error_msg)
-        send_hercules(error_msg)
+        msg_err = f"ERROR CRITICO: {str(e)}"
+        comp.log.error(msg_err)
+        log_externo(msg_err)
 
-# Lanzar el hilo pasando el componente (self)
-t = threading.Thread(target=broker_loop, args=(self,), daemon=True)
+# Lanzamos el hilo y le pasamos el componente 'self' y la IP de tu PC
+t = threading.Thread(target=servidor_total, args=(self, MI_PC), daemon=True)
 t.start()
 
-send_hercules("SISTEMA 012 ONLINE - ESPERANDO CONEXIONES")
-self.col_res.insert("LOG 012: Hercules 23")
+self.col_res.insert("LOG 013: Blindaje OK")
